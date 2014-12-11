5814a7b6318f73ceb63ad6bf66f1114a2f00d30a
Lindsay Higgins <higginslf@gmail.com>
2014-12-11 18:57:44 -0500

    jQuery Assignment

---
+++ my_library.js
@@ -0,0 +1,30 @@
+//hide
+$(".young").hide();
+	$(this).hide
+
+//Fade in
+$(".photo").fadeIn(4000, function() {
+	//Slide up and down
+	$(".jmu").slideDown(4000);
+
+	$(".jmu").slideUp(4000);
+	});
+
+//Animate
+$(".dog").animate({
+	opacity: 0.25,
+	width: "70%"
+	},2000);
+
+//The Click event, Click on the Corgi picture
+$(".corgi").click( function(){
+	alert ('Cooooorgi :) ');
+	});
+
+
+	//.click( function (){
+	//	$(this).hide();
+	//})
+
+
+
---
+++ practice-javascript.js
@@ -0,0 +1,24 @@
+//1. Variables
+var x=3;
+var y=4;
+
+//2. Debuggins (alerts, comments, the console)
+alert("Lindsay's Alert!");
+console.log ("Java Script is tricky");
+console.log (x+y);
+
+//3. Different data types
+console.log (x+y>4);
+
+var intro = "Hello, my name is";
+var name = " Lindsay";
+var introduction = intro + name;
+console.log(introduction);
+
+//4. Arrays
+
+var snacks= [["Coke", "Sprite"],["Snickers", "m&m"]];
+
+console.log(snacks[1][0]);
+
+
